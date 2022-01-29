0450deef7f90f0a435eae813bc467e8b929b4070
Susan <suyvina@gmail.com>
2022-01-29 12:47:34 -0300

    Add Chromedriver Add new class DataType & Operators

---
+++ src/test/java/com/java/DataType.java
@@ -0,0 +1,72 @@
+package com.java;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// Datos primitivos
+
+		/*byte var1 = 1; // numerico entero -28 a 127
+		short var2 = 1; // numerico entero
+		int var3 = 1; // numerico entero
+		long var4 = 1L; // numerico entero
+		float var5 = 1.2f;
+		double var6 = 1.2;
+		char var7 = 'a';
+		boolean var8 = true;
+
+		// Declarar variables
+		int x;
+		x = 10000;
+		int y = 10;
+
+		// Char symbol example
+		char symbol = '\u00A9';
+
+		// cadena char
+		String message = "Hello world";
+
+		// Imprimir en consola
+		System.out.println(symbol);
+		System.out.println(message);
+
+		// concatenacion
+		System.out.println(message + symbol);
+		System.out.println("Susan " + message + symbol);*/
+		
+		// Datos primitivos
+		
+				byte var1 = 1; // Numerico entero
+				short var2 = 1; // Numerico entero
+				int var3 = 1; // Numerico entero (Entero) - Automation
+				long var4 = 1L; // Numerico entero - Automation
+				float var5 = 1.2f; // Numerico real
+				double var6 = 2.223; // Numerico real (decimales) - Automation
+				char var7 = 'a'; // Caracter - Automation
+				boolean var8 = true; // Boleano - Automation - tiene dos valores TRUE / FALSE
+				
+				// Declarar variables
+				int x;		
+				x = 10000;
+				
+				int y = 10;
+				
+				// Char symbol example
+				char symbol = '\u00A9';
+				
+				// Cadena Char
+				String message = "Hello world"; // - Automation
+				
+				// Imprimir en consola
+				System.out.println(symbol);
+				System.out.println(message);
+				
+				// Concatenacion, Se representa con el signo +
+				System.out.println(message+symbol);
+				
+				int numero = 1;
+				String nombre = "Ricardo";
+				System.out.println(nombre + numero);
+
+	}
+
+}
