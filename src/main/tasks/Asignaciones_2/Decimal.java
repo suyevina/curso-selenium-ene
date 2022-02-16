e80efb33a3386d671e61c48f504bea28062955c3
Susan <suyevina@gmail.com>
2022-02-16 08:11:34 -0300

    Act

--- src/main/tasks/Asignaciones_2/Decimal.java
+++ src/main/tasks/Asignaciones_2/Decimal.java
@@ -1,7 +1,9 @@
 package Asignaciones_2;
 
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 public class Decimal {
 
 	public static void main(String[] args) {
@@ -33,7 +35,7 @@ public class Decimal {
 				break;
 				
 			case 2:	
-				deci=areEqualByThreeDecimalPlaces( 3.1756, 3.176);
+				deci=areEqualByThreeDecimalPlaces( 3.175, 3.176);
 				System.out.println(deci);
 				System.out.println("");
 				break;
@@ -60,50 +62,32 @@ public class Decimal {
 
 	public static boolean  areEqualByThreeDecimalPlaces( double x, double y) {
 		double num2=0,num=0;
-	    double a=0, b=0,c=0,d=0, e=0;
+        double a=0, b=0,c=0,d=0, e=0;
 	    boolean  reto=true;
 	    int contador=0;
-//	    Aplicando modulo de un numero
-//	    num=x%1;
-//		 d=x-num; 
-//		 a=num;
-//		 
-//		 num2= y%1;
-//		 e= y-num2;
-//		 b=num2;
-//		 
-//		 if (d==e) {
-//		  for (int i=1; i<=contador;i++) {
-//				num=num*10;
-//				num2=num2*10;
-//				}
-//			 c=num-num2 ;
-//			 if(c < 1 && c>=0) {
-//				 return !reto;
-//			 }
-//		 }
-//		 return reto;			
-	    
-//	    Aplicando Casting
-	    num=(int)x;
-	    num2=(int)y;	    
-	    for(int i=0; i<4;i++) {
-		     if( num==num2) {
-		    	a= x-num;
-		 	    b=y-num2;		 	    
-		    	x=a*10;
-		    	y=b*10;
-		    	num=(int)x;
-		    	num2=(int)y;	
-		    	contador++;	
-		    }   
-	    }
-	    if (contador==3) {
-	    	return reto;
-	    }else {
-	    	return !reto;
-	    }
+
+//Aplicando modulo de un numero
+	    num=x%1;
+		 d=x-num; 
+		 a=num;
+		 
+		 num2= y%1;
+		 e= y-num2;
+		 b=num2;
+		 
+		 if (d==e) {
+		  for (int i=1; i<=contador;i++) {
+				num=num*10;
+				num2=num2*10;
+				}
+			 c=num-num2 ;
+			 if(c < 1 && c>=0) {
+				 return reto;
+			 }
+		 }
+		 return !reto;
 	}
+
 }
-//los decimales con double en la tercera los aproxima y completa con 9 hasta llegar al total de numero permitidos por el tipo de variable preguntar si tiene mas 3 decimales y al llegar al tercero sumar 1
+
 
