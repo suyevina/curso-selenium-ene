120e6587d8bcb4b41d05ff487889b8533bb05e68
Zhussam <svillarroel@kdoce.cl>
2022-02-18 15:00:55 -0300

    AreaCalculator

---
+++ src/main/tasks/ExtraTask/AreaCalculator.java
@@ -0,0 +1,53 @@
+package ExtraTask;
+
+import java.util.Scanner;
+
+public class AreaCalculator {
+
+	public static void main(String[] args) {
+		int bucle ;
+		Calculus calcula= new  Calculus();
+		Scanner opciones = new Scanner(System.in);
+		
+		do {
+			System.out.println("Ingrese el tipo de área a calcular: ");
+			System.out.println("1. Circulo ");
+			System.out.println("2. Rectangulo ");
+			System.out.println("0. Salir");
+			System.out.print("Opción: ");
+			bucle=opciones.nextInt();
+			System.out.println("");
+			
+			switch (bucle) {
+				case 1:
+					System.out.print("Ingrese el valor del radio del circulo:");
+					 calcula.setRadius(opciones.nextDouble());
+					System.out.println("Área del Circulo= "+ calcula.circle());   
+					break;
+				case 2:
+					System.out.println("Ingrese los valores X y Y para calcular su área ");
+					System.out.print("X: ");
+					 calcula.setX(opciones.nextDouble());
+					 System.out.print("Y: ");
+					 calcula.setY(opciones.nextDouble());
+					System.out.println("Área del Rectángulo= "+ calcula.rectangule());   
+					break;
+				case 0:
+					break;
+				default:
+					System.out.println("Opción invalida intente de nuevo");
+					break;
+			}
+		 
+		    
+
+			
+			
+
+			System.out.println("");
+			
+		}while (bucle !=0);
+
+	}
+
+}
---
+++ src/main/tasks/ExtraTask/Calculus.java
@@ -0,0 +1,56 @@
+package ExtraTask;
+
+public class Calculus {
+	double radius;
+	double x;
+	double y;
+	double area;
+
+	public double getArea() {
+		return area;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	public Calculus() {
+		this.radius = radius;
+	
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double circle() {
+		if (radius<0) {
+			return -1.0;
+		}else {
+		area=radius*Math.PI;
+		return area;	
+		}
+	}
+	
+	public double rectangule() {
+		if (x>=0 && y>=0) {
+			area=x*y;
+			return area;
+		}else {
+			return -1.0;
+		}
+	}
+
+}
