ba6a46fdc8bbe51fb217e7d31ab8758674d2d854
Susan <suyevina@gmail.com>
2022-02-20 15:23:59 -0300

    Clase 5

--- src/test/java/com/google/FirstTest.java
+++ src/test/java/com/google/FirstTest.java
@@ -1,11 +1,17 @@
 package com.google;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class FirstTest {
 	
@@ -24,7 +30,25 @@ public class FirstTest {
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 		
-		Assert.assertEquals(driver.getTitle(), "Google");
+		//Implicit wait
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		
+		//Explicit wait
+		WebDriverWait wait = new WebDriverWait(driver,10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[contains(@src,'googlelogo')]")));
+		
+		System.out.println("Paso la prueba");
+
+		//Hard Aassertion
+        Assert.assertEquals(driver.getTitle(), "Google");
+
+//        Soft Assertion:
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(driver.getTitle(), "Google");
+        
+        System.out.println("CheckPoint");
+        
+        softAssert.assertAll();
 
   }
 
